6f1ac15e8d89df92d25150140d802d7556a384cc
Logan Garay <garay.logan@gmail.com>
2020-03-03 23:55:16 -0700

    added mom's section

--- src/views/About.vue
+++ src/views/About.vue
@@ -13,15 +13,23 @@
     </div>
     <div class="row personal">
       <div class="col-5 d-flex align-items-center justify-content-center">
-        <img src="../assets/Jena.jpg" alt="should be Jena" class="JenaImg" />
+        <img src="../assets/Jena.jpg" alt="should be Jena" class="Img" />
       </div>
       <div class="col-6 Jena">
         <p>Jena graduated from the university of Idaho with a Bachelor’s Degree in Psychology and earned her Master’s degree in Social Work from Northwest Nazarene University. Jena is the President of the Idaho Juvenile Justice Association (IJJA), a member of the Association for the treatment of Sexual Abusers (ATSA) and a Senior level certified Juvenile Sexual Offender Treatment Provider (JSOTP). She has over 15 years of experience working with children and their families to address behavioral and mental health concerns. Jena has worked in a variety of settings including in-patient hospitalization, residential treatment, and community-based counseling.</p>
         <p>Jena is passionate about helping youth and their families. Her approach is unique, individualized and effective. She thinks it is important when working with youth to use creative types of therapy that they can relate to, have fun with and will actually help them make long term change. Jena believes that helping individuals make change requires more than focusing on their mental health and emotional well-being. She loves to step outside of the traditional therapy setting and help youth to connect with friends, family and activities that they enjoy which is also a crucial part of becoming healthy and happy.</p>
         <p>Jena has always had a passion for a healthy and active lifestyle. Jena’s husband and her dog, Ike are her adventure buddies and who she spends most of her time with. You will often find Jena exploring the outdoors while camping, running, hiking or snow shoeing. Jena also loves hot yoga, kick boxing and to run half marathons. During down time, Jena can be found meeting a friend for a cup of coffee or cuddled up by the fire place with a good book.</p>
       </div>
-
-      <div class="col-12 Paula"></div>
+    </div>
+    <div class="row">
+      <div class="col-6 offset-1 Paula">
+        <p>Jena graduated from the university of Idaho with a Bachelor’s Degree in Psychology and earned her Master’s degree in Social Work from Northwest Nazarene University. Jena is the President of the Idaho Juvenile Justice Association (IJJA), a member of the Association for the treatment of Sexual Abusers (ATSA) and a Senior level certified Juvenile Sexual Offender Treatment Provider (JSOTP). She has over 15 years of experience working with children and their families to address behavioral and mental health concerns. Jena has worked in a variety of settings including in-patient hospitalization, residential treatment, and community-based counseling.</p>
+        <p>Jena is passionate about helping youth and their families. Her approach is unique, individualized and effective. She thinks it is important when working with youth to use creative types of therapy that they can relate to, have fun with and will actually help them make long term change. Jena believes that helping individuals make change requires more than focusing on their mental health and emotional well-being. She loves to step outside of the traditional therapy setting and help youth to connect with friends, family and activities that they enjoy which is also a crucial part of becoming healthy and happy.</p>
+        <p>Jena has always had a passion for a healthy and active lifestyle. Jena’s husband and her dog, Ike are her adventure buddies and who she spends most of her time with. You will often find Jena exploring the outdoors while camping, running, hiking or snow shoeing. Jena also loves hot yoga, kick boxing and to run half marathons. During down time, Jena can be found meeting a friend for a cup of coffee or cuddled up by the fire place with a good book.</p>
+      </div>
+      <div class="col-5 d-flex align-items-center justify-content-center">
+        <img src="../assets/Jena.jpg" alt="should be Paula" class="Img" />
+      </div>
     </div>
   </div>
 </template>
@@ -68,7 +76,10 @@ export default {
 .Jena p {
   text-align: left;
 }
-.JenaImg {
+.Paula p {
+  text-align: right;
+}
+.Img {
   height: 275px;
   width: 400px;
 }
